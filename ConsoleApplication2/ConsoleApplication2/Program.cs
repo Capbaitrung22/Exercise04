2fd7abb607a9cfd5181aad3f28c8ad49dba975e4
VoMinhHung <ng d  cht m>
2019-10-01 14:35:02 +0700

    float and floor

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,42 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        // input : float x
+        // output: a int that smallest but not smaller than x
+        // idea: first change float x into integer then ++1
+        //       if x is a negative than unchanged
+        //       if x==n then return x
+        public static int Ceil(float x)
+        {
+            int n = (int)x;
+            if (x < 0)
+                n = n;
+            else
+                n = n + 1;
+            
+            return n;
+        }
+        public static int Floor(float x)
+        {
+            int k = (int)x;
+            if (x < 0 )
+            {
+                if (k != x)
+                {
+                    k = k - 1;
+                }
+            }
+            return k;
+        }
+    }
+}
---
+++ ConsoleApplication2/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication2;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCeil()
+        {
+            int result1 = Program.Ceil(2.3f);
+            Assert.AreEqual(3, result1);
+
+            int result2 = Program.Ceil(-2.3f);
+            Assert.AreEqual(-2, result2);
+
+        }
+        [TestMethod]
+        public void TestFloor()
+        {
+            int result1 = Program.Floor((float)2.3);
+            Assert.AreEqual(2, result1);
+
+            int result2 = Program.Floor(-2.3f);
+            Assert.AreEqual(-3, result2);
+        }
+    }
+}
